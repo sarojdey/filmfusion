4dbefa33c25cb728ea22f155b40933d44f443167
SRJ <sarojkumardey.contact@gmail.com>
2024-02-13 01:56:49 +0530

    cast section added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,7 +4,7 @@ import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
 
 import DetailsBanner from "./detailsBanner/DetailsBanner";
-
+import Cast from "./cast/Cast";
 const Details = () => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
@@ -14,7 +14,8 @@ const Details = () => {
 
   return (
     <div>
-      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
     </div>
   );
 };
