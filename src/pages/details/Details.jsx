7b9e64ba9955d812a2083dad42248a2cfe2e02fd
SRJ <sarojkumardey.contact@gmail.com>
2024-02-13 16:34:57 +0530

    official video section added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -5,6 +5,8 @@ import { useParams } from "react-router-dom";
 
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
+
 const Details = () => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
@@ -16,6 +18,7 @@ const Details = () => {
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading}/>
     </div>
   );
 };
